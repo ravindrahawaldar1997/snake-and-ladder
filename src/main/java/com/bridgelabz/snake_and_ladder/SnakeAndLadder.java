1cfe8a4d9c4038807753ece8239722023c698127
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-23 11:13:11 +0530

    Added: Program rolls the die to get the random number between 1 to 6

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program");
         int position = 0;
-        System.out.println("Starting position of the game " +position);
+        System.out.println("Starting position of the game " + position);
+        int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println(randomCheck);
     }
 }
