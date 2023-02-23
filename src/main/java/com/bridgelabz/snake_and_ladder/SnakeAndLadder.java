7b1bcedb668c35cdd391fd5ac04a5bbfe520854c
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-23 12:36:14 +0530

    Added: Program for checking the option of no play ladder or snake

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -4,8 +4,24 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program");
         int position = 0;
-        System.out.println("Starting position of the game " + position);
+        System.out.println("Starting position of the game is " + position);
         int randomCheck = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println(randomCheck);
+        System.out.println("The value of the die is " + randomCheck);
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        switch (option) {
+            case 0:
+                System.out.println("Ladder \nplayer moves ahead by " + randomCheck);
+                position += randomCheck;
+                break;
+            case 1:
+                System.out.println("Snake \nplayer moves behind by " + randomCheck);
+                position -= randomCheck;
+                break;
+            default:
+                System.out.println("No play \nplayer remains in same position");
+                position += 0;
+                break;
+        }
+        System.out.println("The position of the player is " + position);
     }
 }
