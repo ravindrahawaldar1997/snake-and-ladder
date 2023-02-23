8ece3b7820e5a37b6b8aa78a18e33a6a1c1257b2
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-23 10:49:20 +0530

    Added: Welcome message for sake and ladder program

---
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snake_and_ladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Program");
+    }
+}
