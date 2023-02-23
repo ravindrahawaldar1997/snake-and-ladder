0c5e8258f883fd5a06bf82c6e517b69e26672f9b
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-23 11:00:51 +0530

    Added: Program for snake and ladder game starting position is zero

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snake_and_ladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program");
+        int position = 0;
+        System.out.println("Starting position of the game " +position);
     }
 }
